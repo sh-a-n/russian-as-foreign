15e97e414a69f48278bef562f753d56405a3bc51
VITALb91 <vitalb@inbox.ru>
2012-09-03 11:43:15 +0400

    add

--- russian as foreign/GroupingViewController.h
+++ russian as foreign/GroupingViewController.h
@@ -9,11 +9,34 @@
 #import <UIKit/UIKit.h>
 
 @interface GroupingViewController : UIViewController
+{
+    NSMutableArray * first;
+    NSMutableArray * second;
+    NSMutableArray * third;
+    NSMutableArray * sourse;
+}
+
 @property (weak, nonatomic) IBOutlet UINavigationBar *navigationBar;
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *backButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *abortButton;
 @property (weak, nonatomic) IBOutlet UIToolbar *toolBar;
 @property (weak, nonatomic) IBOutlet UIImageView *timerBack;
 @property (weak, nonatomic) IBOutlet UILabel *timerLabel;
-- (IBAction)backButtonSelector:(id)sender;
+@property (weak, nonatomic) IBOutlet UILabel *WordLabel;
+@property (weak, nonatomic) IBOutlet UIButton *Button1;
+@property (weak, nonatomic) IBOutlet UIButton *Button2;
+@property (weak, nonatomic) IBOutlet UIButton *Button3;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *CheckButton;
+@property (weak, nonatomic) IBOutlet UIToolbar *ToolBarGrupps;
+@property (weak, nonatomic) IBOutlet UIButton *RedButton;
+@property (weak, nonatomic) IBOutlet UIButton *BlueButton;
+@property (weak, nonatomic) IBOutlet UIButton *GreenButton;
+@property (weak, nonatomic) IBOutlet UILabel *RedButtonLabel;
+@property (weak, nonatomic) IBOutlet UILabel *BlueButtonLabel;
+@property (weak, nonatomic) IBOutlet UILabel *GreenButtonLabel;
+@property (nonatomic, retain) NSMutableArray * first;
+@property (nonatomic, retain) NSMutableArray * second;
+@property (nonatomic, retain) NSMutableArray * third;
+@property (nonatomic, retain) NSMutableArray * sourse;
+- (IBAction)abortButtonSelector:(id)sender;
 
 @end
--- russian as foreign/GroupingViewController.m
+++ russian as foreign/GroupingViewController.m
@@ -14,10 +14,26 @@
 
 @implementation GroupingViewController
 @synthesize navigationBar;
-@synthesize backButton;
+@synthesize abortButton;
 @synthesize toolBar;
 @synthesize timerBack;
 @synthesize timerLabel;
+@synthesize WordLabel;
+@synthesize Button1;
+@synthesize Button2;
+@synthesize Button3;
+@synthesize CheckButton;
+@synthesize ToolBarGrupps;
+@synthesize RedButton;
+@synthesize BlueButton;
+@synthesize RedButtonLabel;
+@synthesize GreenButton;
+@synthesize BlueButtonLabel;
+@synthesize GreenButtonLabel;
+@synthesize first;
+@synthesize second;
+@synthesize third;
+@synthesize sourse;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -32,22 +48,38 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    [timerBack setFrame:CGRectMake(0, 0, 58, 32)];
-    [timerLabel setFrame:CGRectMake(0, 0, 58, 32)];
+    [timerBack setFrame:CGRectMake(0, 0, 58, 31)];
+    [timerLabel setFrame:CGRectMake(0, 0, 58, 31)];
     [navigationBar setFrame:CGRectMake(58, 0, 422, 38)];
     [toolBar setFrame:CGRectMake(0, 245, 480, 55)];
+    [ToolBarGrupps setFrame:CGRectMake(0, 31, 480, 40)];
     [navigationBar setBackgroundImage:[UIImage imageNamed:@"navigationBar.png"] forBarMetrics:UIBarMetricsDefault];
     [toolBar setBackgroundImage:[UIImage imageNamed:@"tabBar.png"] forToolbarPosition:UIToolbarPositionAny barMetrics:UIBarMetricsDefault];
-
+    /*first = [[NSMutableArray alloc]init];
+    second = [[NSMutableArray alloc]init];
+    third = [[NSMutableArray alloc]init];*/
+    //sourse
 }
 
 - (void)viewDidUnload
 {
     [self setNavigationBar:nil];
-    [self setBackButton:nil];
+    [self setAbortButton:nil];
     [self setToolBar:nil];
     [self setTimerBack:nil];
     [self setTimerLabel:nil];
+    [self setWordLabel:nil];
+    [self setButton1:nil];
+    [self setButton2:nil];
+    [self setButton3:nil];
+    [self setCheckButton:nil];
+    [self setToolBarGrupps:nil];
+    [self setRedButton:nil];
+    [self setBlueButton:nil];
+    [self setGreenButton:nil];
+    [self setBlueButtonLabel:nil];
+    [self setGreenButtonLabel:nil];
+    [self setRedButtonLabel:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -55,10 +87,10 @@
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return ((interfaceOrientation != UIInterfaceOrientationPortrait)&&(interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown));
 }
 
-- (IBAction)backButtonSelector:(id)sender {
+- (IBAction)abortButtonSelector:(id)sender {
     [self.navigationController popViewControllerAnimated:YES];
 }
 @end
